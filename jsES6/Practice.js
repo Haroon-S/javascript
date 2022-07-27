cd74efb3b9b64dac7d76be4aa05ee0b4ab9692ae
Haroon-S <harryshahid420@gmail.com>
2022-07-27 23:48:21 +0500

    ES6+

--- jsES6/Practice.js
+++ jsES6/Practice.js
@@ -89,6 +89,41 @@ function getTempOfTmrw(avgTemperatures) {
 console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
 
 
+// Destructuring objects
+const player = {
+  name: 'Lebron James',
+  club: 'LA Lakers',
+  address: {
+    city: 'Los Angeles'
+  }
+};
+
+// console.log( player.address.city );
+
+const { name, club, address: { city } } = player;
+
+// console.log(`${name} plays for ${club}`);
+
+console.log(`${name} lives in ${city}`);
+
+
+
+const student = {
+  name: "Kyle",
+  age: 24,
+  projects: {
+      diceGame: "Two player dice game using JavaScript"
+  }
+}
+
+
+const {name, age, projects:{diceGame}} = student
+
+console.log(`${name} is ${age} year old. ${name} project is : ${diceGame}`)
+
+
+
+
 
 
 
@@ -306,3 +341,348 @@ import * as capitalizeStrings from "capitalize_strings";
 import subtract from "math_functions";
 
 subtract(7,4);
+
+
+
+
+// Object Literal
+function addressMaker(city, state) {
+  const newAdress = {city, state};
+  
+  console.log(newAdress);
+}
+
+addressMaker('Austin', 'Texas');
+
+
+
+
+
+
+
+// Spread Operator
+let contacts = ["Mary", "Joel", "Danny"];
+
+let personalFriends = [ "David", ...contacts, "Lily" ];
+
+contacts.push("John");
+
+
+let person = {
+    name: "Adam",
+    age: 25,
+    city: "Manchester"
+}
+
+let employee = {
+    ...person,
+    salary: 50000,
+    position: "Software Developer"
+}
+
+console.log(employee);
+
+
+
+
+
+
+
+
+
+
+// includes() (Challenge)
+/*
+    ** includes() Challenge
+        * You want to make a chocolate cake
+        * And you have a list of ingredients represented with an array
+        * Using the JavaScript includes() function
+        * Check if you have the ingredient chocolate in your list of ingredients, and print into the console "We are going to make a chocolate cake" if you have it
+        * Else print into the console "We can't make a chocolate cake because we are missing the ingredient chocolate"
+*/
+
+const listIngredients =  [ "flour", "sugar", "eggs", "butter" ];
+
+if( listIngredients.includes("chocolate")) {
+    console.log("We are going to make a chocolate cake" );
+} else {
+    console.log("We can't make a chocolate cake because we are missing the ingredient chocolate" );
+}
+
+
+
+
+
+
+// **Classes Challenge**:
+
+// Create a class Player with the following:
+// - Add a Name and Country properties
+// - Add a function that when it runs should print into the console ("Messi was born in Argentina");
+// - Make sure to adapt this function to receive dynamic Names and Clubs.
+
+// Create a Subclass called TennisPlayer that extends from the class Player
+// - Add a new property Age.
+// - Add a function that when it runs should print into the console something similar ("Rafael Nadal is 34 years old and knows how to play Tennis");
+// - Make sure the Name and Age are dynamic.
+
+
+
+
+// In this challenge i use 3 different js files 1st for Player class 2nd for Tennis player class and third file for calling everything
+
+// this is Player file
+export class Player
+{
+    constructor(name, country)
+    {
+        this._name = name;
+        this._country = country;
+    }
+    
+    get playerInfo()
+    {
+        return `${this._name} was born in ${this._country}`
+    }
+}
+
+
+// this is TennisPlayer File
+import {Player} from "./Player"
+
+export class TennisPlayer extends Player
+{
+    constructor(name, country, age)
+    {
+        super(name,country);
+        this._age = age;
+    }
+    
+    get playerInfo()
+    {
+        return `${this._name} is ${this._age} years old and knows how to play Tennis`
+    }
+}
+
+
+//  This is index.js file
+import {Player} from "./Player"
+import {TennisPlayer} from "./TennisPlayer"
+
+let tplayer = new TennisPlayer("Novak Djokovic", "Serbia", 35)
+
+console.log(tplayer.playerInfo)
+
+
+
+
+
+
+
+// Promises
+
+
+// Promises
+
+const buyFlightTicket = () => {
+  return new Promise( (resolve, reject) => {
+      setTimeout( () => {
+          const error = false;
+          
+          if( error ) {
+              reject("Sorry your payment was not successful")
+          } else {
+              resolve("Thank you, your payment was successful");
+          }
+      }, 3000)
+  })
+}
+
+buyFlightTicket()
+.then( (success) => console.log(success))
+.catch( (error) => console.log(error) );
+
+
+
+
+
+
+/**
+ * Promises - Challenge
+ * Create a promise that returns some user data and throws an error when not found.
+ * Log the user data when listening to the promise as well as log the error.
+ * 
+ * Docs - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+*/
+
+const data = () => {
+  return new Promise((resolve, reject) =>
+  {
+      setTimeout(() => 
+      {
+          const error = false
+      
+          if(error)
+              {
+                  reject("Sorry the user was not Found")
+          }
+          else{
+                  resolve("The user is found its you Haroon its always been you")
+          }  
+      },5000)
+  })
+}
+
+data()
+.then((found) => console.log(found))
+.catch((notfound) => console.log(notfound))
+
+
+
+// Promises - Challenge 2
+
+const userData = new Promise((resolve, reject) => {
+  const error = false;
+  
+  if(error) {
+      reject('500 Level Error');
+  } else {
+      resolve({
+          firstName: 'Dylan',
+          age: 32,
+          email: 'DylansEmail310@gmail.com'
+      });
+  }
+});
+
+userData
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
+
+
+
+
+
+
+
+
+  /**
+ * Fetch
+ * 
+ * RESTFul API - https://jsonplaceholder.typicode.com/
+ * Docs - https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+ */
+
+// fetch('https://jsonplaceholder.typicode.com/comments/1')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
+fetch('https://jsonplaceholder.typicode.com/comments', {
+  method: "POST",
+  body: JSON.stringify({
+      postId: 1,
+      name: 'Dylan',
+      email: 'dylansemail310@gmail.com',
+      body: 'That was dope!'
+  })
+})
+.then(response => response.json())
+.then(data => console.log(data))
+
+
+
+
+
+
+
+
+/**
+ * Fetch - Challenge
+ * 
+ * GET the first comments value 'https://jsonplaceholder.typicode.com/comments/1' and log its value.
+ * POST a new comment using 'https://jsonplaceholder.typicode.com/comments' and log its value.
+ * 
+ * RESTFul API Guide - https://jsonplaceholder.typicode.com/guide.html
+ * Docs - https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+ */
+
+
+
+ fetch('https://jsonplaceholder.typicode.com/comments', {
+  method : "POST",
+  body : JSON.stringify({
+      postId : 69,
+      userId : 96,
+      title : "Post comment",
+      email : "haroon.gmail.com",
+      body : "This is my first comment"
+  }),
+   headers: {
+  'Content-type': 'application/json; charset=UTF-8',
+},
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  
+  
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+
+
+
+
+  //Challenge - Async & Await
+
+//Print on the console a random joke from the Chuck Norris API using Fetch and Async and Await
+
+const apiUrl = "https://api.chucknorris.io/jokes/random";
+
+async function getJoke() {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    
+    console.log(data.value);
+}
+
+getJoke();
+
+// This challenge is my try it may be invalid
+const apiUrl = "https://api.chucknorris.io/jokes/random";
+
+const randoJoke = async () => {
+    
+    let randomJoke = new Promise((resolve,reject) => {
+    console.log("Hello how are you")
+    setTimeout(() => {
+        fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => response.json())
+        .then(joke => console.log(joke.value))
+        resolve("Here is your joke say ha ha")
+    },5000)
+})
+    const yourJoke = await randomJoke;
+    console.log(yourJoke)
+}
+randoJoke()
+
+
+
+
+
+
+
+
+
+// Set in ES6]
+// it gives us the list
+const exampleSet = new Set([1,1,1,1,2,2,2,2]);
+
+exampleSet.add(5);
+exampleSet.add(5).add(17);
+
+console.log(exampleSet.has(5));
+
+exampleSet.clear();
+
+console.log(exampleSet.size);
